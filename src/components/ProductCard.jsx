99e578547cf8773fb7fe2a8728c3e54e0331edd5
Qamber <qamberlyh@gmail.com>
2024-09-21 15:54:29 +0530

    Edited Product Card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -15,37 +15,41 @@ const ProductCard = ({ product }) => {
             />
           </div>
 
-          <div className="p-2 lg:p-5 sm:p-1 h-[10rem] lg:h-[12rem]">
+          <div className="p-2 lg:p-5 sm:p-1 h-[10rem] lg:h-[12rem] flex flex-col justify-between">
             <h5 className="text-base font-semibold tracking-tight text-gray-900">
-            {((product.title).split(" ").length > 5) ? ((product.title).split(" ").slice(0, 5).join(" ") + "..." ) : (product.title)}
+              {product.title.split(" ").length > 5
+                ? product.title.split(" ").slice(0, 5).join(" ") + "..."
+                : product.title}
             </h5>
 
-            <div className="flex items-center mt-3 justify-between">
-              <span className="text-base lg:text-2xl font-bold text-gray-900">
-                ${product.price}
-              </span>
-            </div>
-
-            <div className="flex items-center justify-between mt-2">
-              <div className="flex">
-                <svg
-                  className="w-5 h-5 text-yellow-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M10 15l-5.878 3.09 1.122-6.545L.244 6.91l6.558-.955L10 0l3.198 5.955 6.558.955-4.782 4.635 1.122 6.545z" />
-                </svg>
-                <span className="ml-2 text-sm font-medium text-gray-500">
-                  {product.rating.rate}
+            <div className="mb-2">
+              <div className="flex items-center mt-3 justify-between">
+                <span className="text-base lg:text-2xl font-bold text-gray-900">
+                  ${product.price}
                 </span>
               </div>
-              <span className="ml-2 text-sm text-gray-400">
-                ({product.rating.count} reviews)
-              </span>
+
+              <div className="flex items-center justify-between mt-2">
+                <div className="flex">
+                  <svg
+                    className="w-5 h-5 text-yellow-400"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M10 15l-5.878 3.09 1.122-6.545L.244 6.91l6.558-.955L10 0l3.198 5.955 6.558.955-4.782 4.635 1.122 6.545z" />
+                  </svg>
+                  <span className="ml-2 text-sm font-medium text-gray-500">
+                    {product.rating.rate}
+                  </span>
+                </div>
+                <span className="ml-2 text-sm text-gray-400">
+                  ({product.rating.count} reviews)
+                </span>
+              </div>
             </div>
           </div>
         </Link>
-        
+
         <AddToCartBtn key={product.id} product={product} />
       </div>
     </>
